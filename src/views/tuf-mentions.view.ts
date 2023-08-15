6e094b82a49fa7f5c0ee13ca527b591800bf9101
Timothé <timothe.frignac@yahoo.fr>
2023-08-15 14:02:23 +0200

    style: Add container around mentions

--- src/views/tuf-mentions.view.ts
+++ src/views/tuf-mentions.view.ts
@@ -1,10 +1,13 @@
 import {LitElement, html, customElement, css} from 'lit-element';
+import {CSS_Global, CSS_Home} from "../styles/ConstructibleStyleSheets";
 
 @customElement('tuf-mentions')
 export class TufMentionsComponent extends LitElement {
 
     //language=css
     static styles = [
+        CSS_Global,
+        CSS_Home,
         css`
         `
     ];
@@ -15,13 +18,16 @@ export class TufMentionsComponent extends LitElement {
 
     render() {
         return html`
-            <h1>Mentions légales</h1>
+            <div class="container">
+            <h3>Mentions légales</h3>
             <p>Le présent site est édité par Thomas Bouvier et Timothé Frignac et hébergé par la société TODO</p>
             <p>Tous doits réservés</p>
-            <h2>Données personnelles</h2>
+            <br>
+            <h4>Données personnelles</h4>
             <p>Votre adresse IP et la référence de la page visitée font partie des informations personnelles enregistrées sur le serveur qui héberge ce site.</p>
             <p>Ces données peuvent éventuellement servir à diagnostiquer un problème, mais elles ne seront jamais transmises à des tiers et elles sont supprimées automatiquement.</p>
             <p>Pour toute question concernant vos données personnelles, vous pouvez nous contacter à l'adresse suivante : TODO</p>
+            </div>
         `;
     }
 
