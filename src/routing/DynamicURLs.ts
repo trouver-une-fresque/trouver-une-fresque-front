4c814c5c8eeba3bf94b5a474a353fd79a063b9a8
Timothé <timothe.frignac@yahoo.fr>
2023-08-15 18:06:48 +0200

    refactor: update DynamicURL

--- src/routing/DynamicURLs.ts
+++ src/routing/DynamicURLs.ts
@@ -4,7 +4,7 @@ import {Strings} from "../utils/Strings";
 export const rechercheDepartementDescriptor = {
     routerUrl: '/dpt:codeDpt-:nomDpt/recherche-:typeRecherche/online-:includesOnline',
     urlGenerator: ({codeDepartement, nomDepartement}: {codeDepartement: string, nomDepartement: string}) => {
-        return ['standard', /* '18_55' */, 'dose_rappel', 'dose_1_enfants', 'dose_1_ou_2'].map(typeRecherche => {
+        return ['standard', 'atelier', 'formation', 'junior'].map(typeRecherche => {
             return `/dpt${codeDepartement}-${Strings.toReadableURLPathValue(nomDepartement)}/recherche-${typeRecherche}/online-non`;
         });
     }
@@ -13,7 +13,7 @@ export const rechercheDepartementDescriptor = {
 export const rechercheCommuneDescriptor = {
     routerUrl: '/dpt:codeDpt-:nomDpt/commune:codeCommune-:codePostal-:nomCommune/recherche-:typeRecherche/en-triant-par-:codeTriCentre/online-:includesOnline',
     urlGenerator: ({codeDepartement, nomDepartement, codeCommune, codePostal, nomCommune, tri}: {codeDepartement: string, nomDepartement: string, codeCommune: string, codePostal: string, nomCommune: string, tri: CodeTriCentre}) => {
-        return ['standard', /* '18_55' */, 'dose_rappel', 'dose_1_enfants', 'dose_1_ou_2'].map(typeRecherche => {
+        return ['standard', 'atelier', 'formation', 'junior'].map(typeRecherche => {
             return `/dpt${codeDepartement}-${Strings.toReadableURLPathValue(nomDepartement)}/commune${codeCommune}-${codePostal}-${Strings.toReadableURLPathValue(nomCommune)}/recherche-${typeRecherche}/en-triant-par-${tri}/online-non`;
         })
     }
