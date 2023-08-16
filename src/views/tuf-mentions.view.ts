8a7e0c780e3371fd6216bace2f8546b2583024a6
Timothé <timothe.frignac@yahoo.fr>
2023-08-16 23:16:16 +0200

    feat: Complete legal terms

--- src/views/tuf-mentions.view.ts
+++ src/views/tuf-mentions.view.ts
@@ -20,13 +20,13 @@ export class TufMentionsComponent extends LitElement {
         return html`
             <div class="container">
                 <h3>Mentions légales</h3>
-                <p>Le présent site est édité par Thomas Bouvier et Timothé Frignac et hébergé par la société TODO</p>
+                <p>Le présent site est édité par <a href="https://github.com/thomas-bouvier">Thomas Bouvier</a> et <a href="https://github.com/TimotF">Timothé Frignac</a> et hébergé par la société Hostinger, dont le siège social est situé HOSTINGER INTERNATIONAL LTD, 61 Lordou Vironos Street, 6023 Larnaca, Chypre, joignable par le moyen suivant : <a href="https://www.hostinger.fr/contact">https://www.hostinger.fr/contact</a>.</p>
                 <p>Tous doits réservés</p>
                 <br>
                 <h4>Données personnelles</h4>
                 <p>Votre adresse IP et la référence de la page visitée font partie des informations personnelles enregistrées sur le serveur qui héberge ce site.</p>
                 <p>Ces données peuvent éventuellement servir à diagnostiquer un problème, mais elles ne seront jamais transmises à des tiers et elles sont supprimées automatiquement.</p>
-                <p>Pour toute question concernant vos données personnelles, vous pouvez nous contacter à l'adresse suivante : TODO</p>
+                <p>Pour toute question concernant vos données personnelles, vous pouvez nous contacter à l'adresse suivante : contact@trouverunefresque.org </p>
             </div>
         `;
     }
