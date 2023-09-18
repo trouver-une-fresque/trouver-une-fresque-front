d90e29f5b584ba579232462344147d023d33f624
Timothé <timothe.frignac@yahoo.fr>
2023-09-18 19:24:29 +0200

    feat: Add workshop Fresque de l'économie circulaire

--- src/state/State.ts
+++ src/state/State.ts
@@ -64,6 +64,7 @@ enum TypeAtelierEnum {
   "FutursProches" = 12,
   "2tonnes" = 100,
   "FresqueClimat" = 200,
+  "FresqueEcoCirculaire" = 300,
 }
 
 export type TypeAtelier = keyof typeof TypeAtelierEnum;
@@ -82,6 +83,7 @@ export type Atelier = {
 };
 
 export const ATELIERS: Record<number, Atelier> = {
+    300: { code: 'FresqueEcoCirculaire', logo: 'logo_fresqueEcoCirculaire.webp', nom: 'Fresque de l\'économie circulaire', promoted: true, website: 'https://www.lafresquedeleconomiecirculaire.com/', styleCode: '_fresqueEcoCirc' },
     200: { code: 'FresqueClimat', logo: 'logo_fresqueClimat.png', nom: 'Fresque du Climat', promoted: true, website: 'https://fresqueduclimat.org/', styleCode: '_fresqueClimat' },
     100: { code: '2tonnes', logo: 'logo_2tonnes.webp', nom: '2tonnes', promoted: true, website: 'https://www.2tonnes.org/', styleCode: '_2tonnes' },
     2: { code: 'FresqueBiodiversite', logo: 'logo_fresqueBiodiversite.svg', nom: 'Fresque de la Biodiversité', promoted: true, website: 'https://www.fresquedelabiodiversite.org/', styleCode: '_fresqueBiodiversite' },
