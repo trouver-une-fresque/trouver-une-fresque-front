08dca1eac32a6928fa081bb5bac5de8fe346ef1f
Timothé Frignac <tim@winnovemed.com>
2024-10-20 17:49:50 +0200

    feat: add card on main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <HelloWorld />
+  <v-container max-width="1200px">
+    <v-card class="search-card">
+      <v-card-title
+        class="d-flex justify-center"
+        max-width="900px"
+      >
+        <h1>Trouvez une Fresque près de chez vous</h1>
+      </v-card-title>
+      <v-card-text class="d-flex justify-center">
+        <v-text-field
+          class="search-bar"
+          label="Localisation"
+          max-width="900px"
+          variant="solo"
+        ></v-text-field>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
   //
 </script>
+
+<style lang="scss" scoped>
+  .search-card {
+    background: linear-gradient(
+      45deg,
+      rgb(var(--v-theme-primary-lighten-1)),
+      rgb(var(--v-theme-secondary))
+    );
+  }
+</style>
